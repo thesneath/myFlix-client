d0cba619b61aa54d0c6cc95a11d20f7d886f72ee
thesneath <71525393+thesneath@users.noreply.github.com>
2021-09-07 16:57:40 -0700

    Updates

--- src.78399e21.js
+++ src.78399e21.js
@@ -52108,7 +52108,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54792" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54808" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -52285,4 +52285,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.jsx"], null)
-//# sourceMappingURL=/src.78399e21.js.map
+//# sourceMappingURL=/myFlix-client/src.78399e21.js.map
